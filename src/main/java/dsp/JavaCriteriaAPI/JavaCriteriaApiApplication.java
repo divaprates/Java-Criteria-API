39260e64652f2497984b238b45e9fde2f6007616
divaprates <divasilvaprates@gmail.com>
2023-11-09 22:09:05 -0300

    commit inicial

---
+++ src/main/java/dsp/JavaCriteriaAPI/JavaCriteriaApiApplication.java
@@ -0,0 +1,13 @@
+package dsp.JavaCriteriaAPI;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaCriteriaApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaCriteriaApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dsp/JavaCriteriaAPI/JavaCriteriaApiApplicationTests.java
@@ -0,0 +1,13 @@
+package dsp.JavaCriteriaAPI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaCriteriaApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
